61cc0c95a54c96869e1425cd344675d6b5e5ad49
Bachra <bechirbouzid@gmail.com>
2017-11-09 09:41:11 -0800

    Homework 1

--- PyPoll.py
+++ PyPoll.py
@@ -3,43 +3,42 @@ import os
 import csv
 
     # Grab the poll csv file
-#poll_CSV = os.path.join('..', "pyPoll",  "raw_data",  'election_data_2.csv')
-
-    # Create new CSV
-    #newWrestlingCSV = os.path.join('..', 'output', 'WWE-Data-' + yearToCheck + '.csv')
-
+poll_CSV = os.path.join('..', 'Python-Challenge', 'election_data_2.csv')
 
     # Set empty list variables :
 Total_Votes = []
-Rogers = []
-Gomez = []
-Brentwood = []
-Higgins = []
+Khan_percent = []
+Correy_percent = []
+Li_percent = []
+OTooley_percent = []
 Winner = []
     
     # open pollcsv file :
     
-#with open(poll_CSV, newline="") as csvfile:
-        #csvreader= csv.reader(csvfile, delimiter=",")
+with open(poll_CSV, newline="") as csvfile:
+        csvreader= csv.reader(csvfile, delimiter=",")
+        mydict = {rows[0]:rows[2] for rows in csvreader}
 
-with open('election_data_2.csv', mode='r') as infile:
-    reader = csv.reader(infile)
-    with open('election_data_2_new.csv', mode='w') as outfile:
-        writer = csv.writer(outfile)
-        mydict = {rows[2]:rows[0] for rows in reader}
+from collections import defaultdict
+Counter = defaultdict(int)
+for v in mydict.values():
+    Counter[v]+=1
 
-#for myKey in mydict.keys():
- #   print "{2}: {0}".format(myKey, len(myDict[myKey])
-#        # Skipp headers
- #       next(csvreader, None)
-        
-#        unique_items = set(csvreader)
- #       keys = [[entry[0]] for entry in unique_items]
-  #      for key in set (keys):
-   #         print("Key '{}' appears {} unique times".format(key, keys.count(key)))
-from collections import Counter
-c = Counter(mydict)
 
-print( c.items() )
 
-        
+# Estimate the percent for each candidate and format
+Khan_percent = Counter['Khan']/len(mydict.keys())
+Correy_percent = Counter['Correy']/len(mydict.keys())
+Li_percent = Counter['Li']/len(mydict.keys())
+OTooley_percent = Counter["O'Tooley"]/len(mydict.keys())
+
+# Print final results
+
+print("---Election Results-------")
+print(" Total Votes : " + str(len(mydict.keys())))
+print("----------------------")
+print("Khan : " + str("{:.1%}".format(Khan_percent))+ "  ("+str(Counter["Khan"])+")")
+print("Correy : " + str("{:.1%}".format(Correy_percent))+ "  ("+str(Counter["Correy"])+")")
+print("Li : "  + str("{:.1%}".format(Li_percent))+ "  ("+str(Counter["Li"])+")")
+print("O'Tooley : " +  str("{:.1%}".format(OTooley_percent))+ "  ("+str(Counter["O'Tooley"])+")")
+
